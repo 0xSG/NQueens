e510e13f2c9831409e2f15641ac4aa0353e9bb7b
sooryagangarajk <sooryagangaraj@gmail.com>
2018-05-06 20:17:28 +0530

    First commit

---
+++ NQueens.py
@@ -0,0 +1,49 @@
+
+n = input("Enter the no of queens:")
+board =[[],[],[],[]]
+resList=[]
+def findQP(count):
+    global n,resList,board
+    if(count==0):
+        return 1
+    elif(count<0):
+        return 0
+    for k in range(0, n):
+        if k not in board[0]:
+            for l in range(0, n):
+                if l not in board[1]:
+
+                    if (l - k) not in board[2] and (l + k) not in board[3]:
+
+                        board[0].append(k)
+                        board[1].append(l)
+                        board[2].append(l - k)
+                        board[3].append(k + l)
+                        res = findQP(count - 1)
+
+                        if (res == 0):
+                            board[0].pop()
+                            board[1].pop()
+                            board[2].pop()
+                            board[3].pop()
+
+                        elif res==1:
+                            resList+=[(k,l)]
+                            return 1
+    else:
+        return 0
+
+if(findQP(n)):
+    print (resList)
+
+else:
+    print ("No")
+
+
+
+
+
+
+
+
+
